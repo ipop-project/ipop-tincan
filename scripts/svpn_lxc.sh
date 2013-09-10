2cf12f5fabb155172708badcaee92ae69b84271d
Pierre St Juste <ptony82@gmail.com>
2013-09-10 06:58:30 -0400

    Simplified script, removed lxc network configuration

--- scripts/svpn_lxc.sh
+++ scripts/svpn_lxc.sh
@@ -2,71 +2,44 @@
 # this script uses lxc to run multiple instances of SocialVPN
 # this script is designed for Ubuntu 12.04 (64-bit)
 #
-# for VM 1
-# svpn_lxc.sh username password host 1 10 30 10.0.3 gvpn"
-#
-# for VM 2
-# svpn_lxc.sh username password host 11 20 30 10.0.4 gvpn"
-#
+# usage: svpn_lxc.sh username password host 1 10 svpn"
 
 USERNAME=$1
 PASSWORD=$2
 XMPP_HOST=$3
 CONTAINER_START=$4
 CONTAINER_END=$5
-WAIT_INTERVAL=$6
-LXC_NETWORK_BASE=$7
-MODE=$8
+MODE=$6
 HOST=$(hostname)
 IP_PREFIX="172.16.5"
-
+CONTROLLER=gvpn_controller.py
 START_PATH=container/rootfs/home/ubuntu/start.sh
-LXC_CONFIG_PATH=/etc/default/lxc
 
 sudo apt-get update
 sudo apt-get install -y lxc tcpdump
 
 wget -O ubuntu.tgz http://goo.gl/Ze7hYz
 wget -O container.tgz http://goo.gl/XJgdtf
-wget -O svpn.tgz http://goo.gl/1nmORG
+wget -O svpn.tgz http://goo.gl/2L6nNH
 
 sudo tar xzf ubuntu.tgz; tar xzf container.tgz; tar xzf svpn.tgz
 sudo cp -a ubuntu/* container/rootfs/
 sudo mv container/home/ubuntu container/rootfs/home/ubuntu/
 mv svpn container/rootfs/home/ubuntu/svpn/
 
-cat > lxc-net.cfg << EOF
-LXC_AUTO="true"
-USE_LXC_BRIDGE="true"
-LXC_BRIDGE="lxcbr0"
-LXC_ADDR="$LXC_NETWORK_BASE.1"
-LXC_NETMASK="255.255.255.0"
-LXC_NETWORK="$LXC_NETWORK_BASE.0/24"
-LXC_DHCP_RANGE="$LXC_NETWORK_BASE.2,$LXC_NETWORK_BASE.254"
-LXC_DHCP_MAX="253"
-LXC_SHUTDOWN_TIMEOUT=120
-
-EOF
-
-sudo cp lxc-net.cfg $LXC_CONFIG_PATH
-sudo service lxc-net restart
-sudo tcpdump -i eth0 -w svpn_$HOST.cap &> /dev/null &
+if [ "$MODE" == "svpn" ]
+then
+    CONTROLLER=svpn_controller.py
+fi
 
 cat > $START_PATH << EOF
 #!/bin/bash
 SVPN_HOME=/home/ubuntu/svpn
 CONFIG=\`cat \$SVPN_HOME/config\`
 \$SVPN_HOME/svpn-jingle &> \$SVPN_HOME/svpn_log.txt &
+python \$SVPN_HOME/$CONTROLLER \$CONFIG &> \$SVPN_HOME/controller_log.txt &
 EOF
 
-if [ "$MODE" == "gvpn" ]
-then
-    echo -n "python \$SVPN_HOME/gvpn_controller.py " >> $START_PATH
-else
-    echo -n "python \$SVPN_HOME/vpn_controller.py " >> $START_PATH
-fi
-
-echo " \$CONFIG &> \$SVPN_HOME/controller_log.txt &" >> $START_PATH
 chmod 755 $START_PATH
 
 for i in $(seq $CONTAINER_START $CONTAINER_END)
@@ -77,11 +50,11 @@ do
 
     sudo cp -a container $container_name
 
-    if [ "$MODE" == "gvpn" ]
+    echo -n "$USERNAME $PASSWORD $XMPP_HOST $IP_PREFIX.$i" > \
+             $container_name/rootfs/home/ubuntu/svpn/config
+
+    if [ "$MODE" == "svpn" ]
     then
-        echo -n "$USERNAME $PASSWORD $XMPP_HOST $IP_PREFIX.$i" > \
-                 $container_name/rootfs/home/ubuntu/svpn/config
-    else
         echo -n "$USERNAME $PASSWORD $XMPP_HOST" > \
                  $container_name/rootfs/home/ubuntu/svpn/config
     fi
@@ -90,9 +63,6 @@ do
     sudo echo "lxc.rootfs = $container_path/rootfs" >> $container_path/config
     sudo echo "lxc.mount = $container_path/fstab" >> $container_path/config
     sudo lxc-start -n $container_name -d
-    sleep $WAIT_INTERVAL
+    sleep 30
 done
-
-sleep 7200
-sudo pkill tcpdump
 
