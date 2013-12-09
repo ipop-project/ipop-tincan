6d5bd0b4cec08bbf9e49f28405be01201177dcd2
Pierre St Juste <ptony82@gmail.com>
2013-12-08 23:51:51 -0500

    Very minor aesthetic change

--- src/tincanconnectionmanager.cc
+++ src/tincanconnectionmanager.cc
@@ -105,7 +105,7 @@ void TinCanConnectionManager::Setup(
   }
   int error = 0;
 #if defined(LINUX) || defined(ANDROID)
-  error = tap_set_ipv4_addr(ip4.c_str(), ip4_mask);
+  error |= tap_set_ipv4_addr(ip4.c_str(), ip4_mask);
   error |= tap_set_ipv6_addr(ip6.c_str(), ip6_mask);
   error |= tap_set_mtu(MTU) | tap_set_base_flags() | tap_set_up();
 #endif
